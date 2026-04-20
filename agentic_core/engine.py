00079a409adddfeb6c3748a9f40866c21d6918ac
nguyenv <182782819+nguyenv217@users.noreply.github.com>
2026-04-21 00:04:54 +0800

    doc: better documentation for RunnerConfig

--- agentic_core/engine.py
+++ agentic_core/engine.py
@@ -16,15 +16,28 @@ logger = logging.getLogger(__name__)
 
 @dataclass
 class RunnerConfig:
+    '''
+    Configuration for the AgentRunner.
+
+    Args:
+        max_iterations: The maximum number of iterations the agent can take before failing.
+        system_prompt: A prompt to be used as the system prompt for the agent.
+        tools: A list of tools to be used by the agent. This includes both non-MCP and MCP tools of your choice. MCP tools included here but not loaded in last turns must be specified in `mcp_preload_tools` also to initialize properly
+        toolset: The name of a preconfigured `toolset` registered with tool_manager. Passing `tools` will take priority over this settings to encourage clearer tooling injection.
+        clear_loaded_tool: Whether to keep the last turn loaded MCP tools
+        mcp_active_servers: A list of MCP server names to be used for the agent. This is useful when you only want to use a specific set of servers. It is best accompanied with `mcp_preload_tools` and `enable_mcp_discovery=False` to conserve resources.
+        mcp_preload_tools: A list of MCP tool names to be preloaded for the agent. This is useful when you know what MCP tools you want to use. 
+        enable_mcp_discovery: Whether to enable user to dynamically browse and load MCP tools. Recommended 'False' if `mcp_preload_tools` is specified
+    '''
     max_iterations: int = 20
     system_prompt: str | None = None
-    tools: list[ToolSchema] | None = None        # BOTH none-MCP and MCP tools. MCP tools included here but not loaded in last turns must be specified in `mcp_preload_tools` also to initialize properly
-    toolset: str | None = None                   # Additionally, specify a preconfigured `toolset` registered with tool. Passing `tools` will take priority over this settings to encourage clearer tooling injection.
+    tools: list[ToolSchema] | None = None        
+    toolset: str | None = None                   
     # MCP configs
-    clear_loaded_tool: bool = True               # Whether to keep the last turn loaded MCP tools
+    clear_loaded_tool: bool = True               
     mcp_active_servers: list[str] | None = None  # e.g. ["github", "memory"]. Supply this before supplying mcp_preload_tools.
     mcp_preload_tools: list[str] | None = None   # e.g. ["github_create_issue"]
-    enable_mcp_discovery: bool = False           # Whether to enable user to dynamically browse and load MCP tools. Recommended 'False' if `mcp_preload_tools` is specified.  
+    enable_mcp_discovery: bool = False           
 
     def __post_init__(self):
         if self.max_iterations < 1: 
